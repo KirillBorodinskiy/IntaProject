4978b001cf2fb04ad49926d0012b99c8f3e08fec
KirillBorodinskiy <kirill.borodinskiy@gmail.com>
2024-05-11 17:32:12 +0200

    Working a-j and 1-10
    Co-authored-by: Frolov, Roman <frolorom@fjfi.cvut.cz>

--- project/application/views.py
+++ project/application/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    r = range(11)
+    n = [0,1,2,3,4,5,6,7,8,9,10]
+    l = ['z','a','b','c','d','e','f','g','h','i','j']
+    rn = list(zip(r, n))
+    rl = list(zip(r, l))
     context = {
-        'range':range(10)
+        'rn': rn,
+        'rl': rl,
+        'zi': list(zip(r,n,l)),
+        'range':r,
     }
     return render(request, 'application/index.html',context)
